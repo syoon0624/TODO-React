b5e18164d7a1a0dbc6e53df48084dabe7a9d97e3
seungyoonLee <syoon624@naver.com>
2022-12-19 21:05:08 +0900

    butiful-dnd를 활용한 drag & drop 기능

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,23 +1,7 @@
 import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 export default function Lists({ todoData, setTodoData }) {
-  const btnStyle = {
-    color: '#fff',
-    border: 'none',
-    padding: '5px 9px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right',
-  };
-
-  const listStyle = (completed) => {
-    return {
-      padding: '10px',
-      borderBottom: '1px #ccc dotted',
-      textDecoration: completed ? 'line-through' : 'none',
-    };
-  };
-
   const handleClick = (id) => {
     let newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
@@ -33,29 +17,67 @@ export default function Lists({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
+  const handleEnd = (result) => {
+    if (!result.destination) return;
+    const newTodoData = [...todoData];
+
+    const [reorederedItem] = newTodoData.splice(result.source.index, 1);
+
+    newTodoData.splice(result.destination.index, 0, reorederedItem);
+    setTodoData(newTodoData);
+  };
+
   return (
     <div>
-      {todoData.map((data) => (
-        <div key={data.id}>
-          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
-            <div className="items-center">
-              <input
-                type="checkbox"
-                defaultChecked={data.completed}
-                onChange={() => handleCompleteChange(data.id)}
-              />{' '}
-              <span className={data.completed && 'line-through'}>
-                {data.title}
-              </span>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="to-dos">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div>
+                      <div
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        className={`${
+                          snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'
+                        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+                      >
+                        <div className="items-center">
+                          <input
+                            type="checkbox"
+                            defaultChecked={data.completed}
+                            onChange={() => handleCompleteChange(data.id)}
+                          />{' '}
+                          <span
+                            className={
+                              data.completed ? 'line-through' : undefined
+                            }
+                          >
+                            {data.title}
+                          </span>
+                        </div>
+                        <div className="items-center">
+                          <button onClick={() => handleClick(data.id)}>
+                            X
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-            <div className="items-center">
-              <button style={btnStyle} onClick={() => handleClick(data.id)}>
-                X
-              </button>
-            </div>
-          </div>
-        </div>
-      ))}
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
